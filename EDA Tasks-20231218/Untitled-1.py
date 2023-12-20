d7e46d35ec8c58281e091f71fb8bdd6972e00b9e
barc <barc>
2023-12-20 10:12:37 +0200

    .

---
+++ EDA Tasks-20231218/Untitled-1.py
@@ -0,0 +1,52 @@
+# %%
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+# %%
+DirPath = os.getcwd()
+DataPath = os.path.join(DirPath,'AmesHousing.csv')
+df = pd.read_csv(DataPath) 
+display("df.describe:",df.describe())
+display("---------------------------")
+display("df.info:",(df.info()))
+display("---------------------------")
+display("df.head:",df.head())
+display("---------------------------")
+display("df.shape:",df.shape)
+
+
+# %%
+data = df[["SalePrice","PID","Lot Frontage"]]
+data.dropna().head()
+df.columns
+
+# %%
+numerical = [
+  'SalePrice', 'Lot Area', 'Overall Qual', 'Overall Cond', '1st Flr SF', '2nd Flr SF', 'Bedroom AbvGr']
+
+categorical = [
+  'MS Zoning', 'Lot Shape', 'Neighborhood', 'Central Air', 'Sale Condition', 'Mo Sold', 'Yr Sold']
+
+
+# %%
+housing = df[numerical + categorical]
+housing.info()
+
+# %%
+sns.set(style= "whitegrid", palette="pastel", font_scale=1.1,
+        rc={"figure.figsize": [8,5]})
+
+sns.distplot(housing['SalePrice'], norm_hist=False, kde=False, bins=20, hist_kws={"alpha": 1}
+).set(xlabel='Sale Price', ylabel='Count');
+
+housing[numerical].hist(figsize=(15,15), bins= 20 , layout= (3,4));
+# %%
+sns.countplot(housing[categorical]['MS Zoning'])
+# %%
+housing[categorical]
+for i in housing[categorical]:
+    sns.countplot(i)
